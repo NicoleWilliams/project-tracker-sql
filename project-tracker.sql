51bdcf5db0be47a67722fce0bfbb2409c84aeaaf
Nicole Williams <nicole.boesenberg@gmail.com>
2023-07-27 20:34:48 -0700

    completed project tracker

--- project-tracker.sql
+++ project-tracker.sql
@@ -7,15 +7,54 @@
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
+SET xmloption = content;
 SET client_min_messages = warning;
-SET search_path = public, pg_catalog;
+SET row_security = off;
 
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
+
+--
+-- Name: grades; Type: TABLE; Schema: public; Owner: nicolewilliams
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO nicolewilliams;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: nicolewilliams
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO nicolewilliams;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: nicolewilliams
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
 
 --
 -- Name: projects; Type: TABLE; Schema: public; Owner: nicolewilliams
@@ -44,6 +83,25 @@ CREATE TABLE public.students (
 ALTER TABLE public.students OWNER TO nicolewilliams;
 
 --
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: nicolewilliams
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: nicolewilliams
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdevelops	Markov	50
+4	sdevelops	Blockly	100
+\.
+
+
+--
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: nicolewilliams
 --
 
@@ -67,6 +125,21 @@ sdevelops	Sarah	Developer
 
 
 --
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: nicolewilliams
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 5, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: nicolewilliams
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: projects projects_pkey; Type: CONSTRAINT; Schema: public; Owner: nicolewilliams
 --
 
@@ -80,6 +153,22 @@ ALTER TABLE ONLY public.projects
 
 ALTER TABLE ONLY public.students
     ADD CONSTRAINT students_pkey PRIMARY KEY (github);
+
+
+--
+-- Name: grades grades_project_title_fkey; Type: FK CONSTRAINT; Schema: public; Owner: nicolewilliams
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_project_title_fkey FOREIGN KEY (project_title) REFERENCES public.projects(title);
+
+
+--
+-- Name: grades grades_student_github_fkey; Type: FK CONSTRAINT; Schema: public; Owner: nicolewilliams
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_student_github_fkey FOREIGN KEY (student_github) REFERENCES public.students(github);
 
 
 --
